9155e63687b0e6cf848f489dbfa7dde799cf3252
rthew1 <rthew1@gmail.com>
2018-04-13 12:06:30 -0400

    UPDATE

--- SortIntDriver.cpp
+++ SortIntDriver.cpp
@@ -11,19 +11,58 @@ Description:  Print and sort array
 #include "SortInt.h"
 using namespace std;
 
+enum Attribute { ACC = 0, AGI, BAL, SPD, STA, STR, AGG, ANT, BRA, CRE, DET, FLA, INF, TEA, WOR, CHE, DEF, DEK, FAC, HIT, GTO, PAS, POK, POS, SLA, STI ,WRI};
+const char* getAttribute(enum Attribute attribute)
+{
+	switch (attribute)
+	{
+	case ACC: return "ACC";
+	case AGI: return "AGI";
+	case BAL: return "BAL";
+	case SPD: return "SPD";
+	case STA: return "STA";
+	case STR: return "STR";
+	case AGG: return "AGG";
+	case ANT: return "ANT";
+	case BRA: return "BRA";
+	case CRE: return "CRE";
+	case DET: return "DET";
+	case FLA: return "FLA";
+	case INF: return "INF";
+	case TEA: return "TEA";
+	case WOR: return "WOR";
+	case CHE: return "CHE";
+	case DEF: return "DEF";
+	case DEK: return "DEK";
+	case FAC: return "FAC";
+	case HIT: return "HIT";
+	case GTO: return "GTO";
+	case PAS: return "PAS";
+	case POK: return "POK";
+	case POS: return "POS";
+	case SLA: return "SLA";
+	case STI: return "STI";
+	case WRI: return "WRI";
+	}
+}
+
 int main()
 {
 	STACK s;
-	int val, max = 5;
+	Attribute theAttribute;
+	int val, max = 27;
 	for (int i = 0; i < max; i++)
 	{
-		cout << "data#" << i+1 <<": ";
+		theAttribute = Attribute(i);
+		cout << getAttribute(theAttribute) <<": ";
 		cin >> val;
 		s.push(val);
 	}
 
 	s.print();
 	s.printStats();
+
+	cout << ACC << endl;
 
 	system("pause");
 	return 0;
